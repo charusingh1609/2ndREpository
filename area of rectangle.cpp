3d5ddc29255fee64e3a1066add4e23df4c739e5f
charusingh1609 <singhcharu359@gmail.com>
2021-05-20 17:46:48 +0530

    Area of rectangle program in cpp

---
+++ area of rectangle.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int l;
+	int b;
+	int area;
+	printf("enter lemgth value : ");
+	scanf("%d",&l);
+	printf("enter breath value: ");
+	scanf("%d",&b);
+	area=l*b;
+	printf("area of rectangle is : %d",  area);
+	}
