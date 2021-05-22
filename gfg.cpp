9f3caaa74320a95ac8b59db860bc3dedd114d8e6
charusingh1609 <singhcharu359@gmail.com>
2021-05-22 23:06:04 +0530

    problem of the day soln of gfgs

---
+++ gfg.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int count=0;
+	int i;
+	int j;
+	int a[i];
+	int b[j];
+	int m;
+	int n;
+	for(i=0;i<=n;i++){
+		for(j=3;j<=m;j++){
+			if(a[i]==b[j]){
+				count=count+1;
+			}
+			return count;
+		}
+	}
+	
+}
